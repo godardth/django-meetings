8542e13b36cb50bc0f811994d237a9fb94982634
Theophile Godard <theo.godard@gmail.com>
2018-02-26 16:37:51 +0900

    Fixing deployment bug related to fixed assets

--- momeetings/settings.py
+++ momeetings/settings.py
@@ -124,7 +124,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
