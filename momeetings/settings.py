6c81419dc72a4d03cc9b4fef58a300b10337bd74
Theophile Godard <theo.godard@gmail.com>
2018-02-26 16:51:55 +0900

    Configure the database persistency

--- momeetings/settings.py
+++ momeetings/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 AUTH_PASSWORD_VALIDATORS = [
