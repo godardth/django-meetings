19644e192e57fa2a8fe94fd29384ff64c93c8b11
Theophile Godard <theo.godard@gmail.com>
2018-02-26 19:10:18 +0900

    Creating the views and serializers of basic items

--- minutes/views.py
+++ minutes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from tutorial.quickstart.serializers import UserSerializer, ComSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# class CompanyViewSet(viewsets.ModelViewSet):
+#     queryset = Company.objects.all()
+#     serializer_class = CompanySerializer
+#
+#
+# class PersonViewSet(viewsets.ModelViewSet):
+#     queryset = Person.objects.all()
+#     serializer_class = PersonSerializer
