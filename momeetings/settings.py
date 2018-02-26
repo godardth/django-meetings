a7df8aab453601f1b9ad3d00aa610a35f5ab54b5
Theophile Godard <theo.godard@gmail.com>
2018-02-26 17:54:50 +0900

    Fixing the database

--- momeetings/settings.py
+++ momeetings/settings.py
@@ -79,14 +79,13 @@ WSGI_APPLICATION = 'momeetings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES = {}
 DATABASES['default'].update(db_from_env)
 
 AUTH_PASSWORD_VALIDATORS = [
