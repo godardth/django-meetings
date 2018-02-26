d090d728bbf819bb713a9d03ac6c887ead7afb66
Theophile Godard <theo.godard@gmail.com>
2018-02-26 16:19:09 +0900

    Updating the gitignore to ignore pycharm files

--- momeetings/settings.py
+++ momeetings/settings.py
@@ -76,13 +76,16 @@ WSGI_APPLICATION = 'momeetings.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
